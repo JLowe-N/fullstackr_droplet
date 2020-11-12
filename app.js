f335024ed0f58cffe0445b8d39156017d4ca6120
jlowen <Justin.G.Lowen@gmail.com>
2020-11-12 17:05:15 -0600

    Add temp response file

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const fileSystem = require('fs');
+const path = require('path');
 const port = 3000;
 
 app.get('/', (req, res) => {
@@ -7,6 +9,19 @@ app.get('/', (req, res) => {
 	res.send('Hello World!');
 
 });
+
+app.get('/.well-known/pki-validation/A77313B52F2D555618AE53C29CDABCDE.txt', (req, res) => {
+	const filePath = path.join('/home/jlowen/.well-known/pki-validation', 'A77313B52F2D555618AE53C29CDABCDE.txt')
+	const stat = fileSystem.statSync(filePath)
+
+	response.writeHead(200, {
+		'Content-Type': 'text/plain',
+		'Content-Length': stat.size
+	})
+
+	const readStream = fileSystem.createReadStream(filePath);
+	readStream.pipe(response)
+})
 
 
 app.get('/tea', (req, res) => {
